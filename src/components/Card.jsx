a753dad0a5edf178ea67e3a88b59d7c2288a196b
Aman jain <amjaanin@gmail.com>
2024-02-08 12:44:19 +0530

    next change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import { FaRegFileAlt} from "react-icons/fa"
+import {LuDownload} from "react-icons/lu"
 
-function Card() {
+function Card({data}) {
   return (
-    <div className='relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+    <div className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden m-3'>
         <FaRegFileAlt />
-        <p className='text-sm mt-5 leading-tight font-semibold'>Lorem, ipsum dolor sit amet consectetur adipisicing.</p>
-        <div className='footer absolute bottom-0 bg-sky-200 w-full h-10 py-3 left-0'>
-
+        <p className='text-sm mt-5 leading-tight font-semibold'>{data.desc}</p>
+        <div className='footer absolute bottom-0 w-full left-0'>
+            <div className='flex items-center justify-between px-8 py-3 mb-2'>
+                <h5>0.4mb</h5>
+                <span className='w-7 h-7 bg-zinc-700 rounded-full flex items-center justify-center cursor-pointer'>
+                <LuDownload size=".7em" color="#fff"/>
+                </span>
+            </div>
+            <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
+                <h3 className='text-sm font-semibold'>Download Now</h3>
+            </div>
         </div>
      </div>
   )
